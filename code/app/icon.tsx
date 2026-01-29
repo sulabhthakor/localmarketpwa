c0247b72ee3596356ed860424ec541cfed2ac747
sulabhthakor <sulabhthakor@gmail.com>
2026-01-29 10:33:40 +0530

    PWA Icon Updasted

---
+++ code/app/icon.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+export default function Icon() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#fe330a',
+                    borderRadius: '20%',
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#ffffff"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    {/* Lucide Store icon path */}
+                    <path d="m2 7 4.41-4.41A2 2 0 0 1 7.83 2h8.34a2 2 0 0 1 1.42.59L22 7" />
+                    <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+                    <path d="M15 22v-4a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2v4" />
+                    <path d="M2 7h20" />
+                    <path d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 10a2 2 0 0 1-2-2V7" />
+                </svg>
+            </div>
+        ),
+        {
+            ...size,
+        }
+    )
+}
--- code/app/manifest.ts
+++ code/app/manifest.ts
@@ -11,21 +11,21 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#fe330a',
         icons: [
             {
-                src: '/icon-192.png',
+                src: '/api/icons/192',
                 sizes: '192x192',
                 type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icon-512.png',
+                src: '/api/icons/512',
                 sizes: '512x512',
                 type: 'image/png',
                 purpose: 'maskable',
             },
             {
-                src: '/icon.svg',
-                sizes: 'any',
-                type: 'image/svg+xml',
+                src: '/api/icons/512',
+                sizes: '512x512',
+                type: 'image/png',
                 purpose: 'any',
             },
         ],
